3dfd9f0b53943422c99b79e756f97af63e495e9a
wsnipex <wsnipex@a1.net>
2013-07-26 15:17:01 +0200

    [packaging] add debug packages and use lto

--- debian/prepare-vnsiserver.sh
+++ debian/prepare-vnsiserver.sh
@@ -7,9 +7,11 @@ tag=$(awk '/[0-9]-([0-9]+)~/ {match($2, "[0-9]-([0-9]+)~",a)} END{print a[1]}' c
 
 version="1:"$(awk -F'=' '/\*VERSION/ {gsub("\"",""); gsub(" ",""); gsub(";",""); print $2}' ../vdr-plugin-vnsiserver/vnsi.h)"-${tag}~${dist}"
 echo "${version}" > ${package}.version
+echo "${version}" > ${package}-dbg.version
 
 dch -c ${package}.changelog --create --empty --package vdr-plugin-vnsiserver -v"${version}" --distribution ${dist} --force-distribution 2>/dev/null "no upstream changelog available"
 
 awk '/xbmc-pvr-vdr-vnsi/ {gsub("\\(",""); gsub("\\)",""); print $2}' changelog > xbmc-pvr-vdr-vnsi.version
+cp xbmc-pvr-vdr-vnsi.version xbmc-pvr-vdr-vnsi-dbg.version
 
 cd ..
