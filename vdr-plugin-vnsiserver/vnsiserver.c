cee3c4776642a1710e712d9de666c7d63b9d96c8
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2013-11-12 17:21:05 +0100

    sync with upstream 54a6bd9f

--- vdr-plugin-vnsiserver/vnsiserver.c
+++ vdr-plugin-vnsiserver/vnsiserver.c
@@ -159,6 +159,7 @@ void cVNSIServer::NewClientConnected(int fd)
   int val = 1;
   setsockopt(fd, SOL_SOCKET, SO_KEEPALIVE, &val, sizeof(val));
 
+#ifdef SOL_TCP
   val = 30;
   setsockopt(fd, SOL_TCP, TCP_KEEPIDLE, &val, sizeof(val));
 
@@ -170,6 +171,7 @@ void cVNSIServer::NewClientConnected(int fd)
 
   val = 1;
   setsockopt(fd, SOL_TCP, TCP_NODELAY, &val, sizeof(val));
+#endif
 
   INFOLOG("Client with ID %d connected: %s", m_IdCnt, cxSocket::ip2txt(sin.sin_addr.s_addr, sin.sin_port, buf));
   cVNSIClient *connection = new cVNSIClient(fd, m_IdCnt, cxSocket::ip2txt(sin.sin_addr.s_addr, sin.sin_port, buf));
@@ -211,7 +213,11 @@ void cVNSIServer::Action(void)
   }
   else
   {
+#if VDRVERSNUM >= 20102
+    cmd = cString::sprintf("rm -f %s/*.vnsi", cVideoDirectory::Name());
+#else
     cmd = cString::sprintf("rm -f %s/*.vnsi", VideoDirectory);
+#endif
   }
   int ret = system(cmd);
 
